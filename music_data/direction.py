6c39f8a1b96e629eb7b9de84f7a7cd7e349a592d
fredrik wallberg <fredrik.wallberg@gmail.com>
2014-05-16 10:52:50 +0200

    Associate directions with note events. Register directions using their classes, in order to be able to sort the Register later.

--- music_data/direction.py
+++ music_data/direction.py
@@ -19,5 +19,35 @@ class DirectionMixin(object):
         else:
             return 0
 
-            
+    @property
+    def direction_content(self):
+        result = []
+        for type_ in self.direction_type[:]:
+            result.append(type_.content()[0])
+        return result
+
 # offset = ? avoid name conflicts
+
+class DirectionsRegister(object):
+
+    def __init__(self, elements=None):
+        if elements is not None:
+            self.elements = elements
+        else:
+            self.elements = {}
+
+    def append(self, direction):
+        for elt in direction.direction_content:
+            cls = elt.__class__.__name__
+            if cls in self.elements:
+                lst = self.elements[cls]
+                lst.append(elt)
+            else:
+                lst = []
+                lst.append(elt)
+                self.elements[cls] = lst
+            
+    def extend(self, elts):
+        for elt in elts:
+            self.append(elt)
+
---
+++ part/associate_directions_with_note_events.py
@@ -0,0 +1,66 @@
+from measure.measure import Measure
+
+def is_note_or_chord(elt):
+    from resources.musicxml import note
+    from music_data.chord_container import ChordContainer
+    return isinstance(elt, note) or isinstance(elt, ChordContainer)
+
+def is_direction(elt):
+    from resources.musicxml import direction
+    return isinstance(elt, direction)
+
+def partition_by_offset(elts, offset):
+    lesser, greater, = [], []
+    for elt in elts:
+        if elt.offset <= offset:
+            lesser.append(elt)
+        else:
+            greater.append(elt)
+    return lesser, greater
+
+def associate_directions_with_note_events(measure_list):
+    from copy import deepcopy
+    from music_data.direction import DirectionsRegister
+
+    # Every element in result which is not a top-level musicevent
+    # (i.e. a Note or rest), needs to be attached/appended to a
+    # top-level musicevent.
+    new_measure_list = []
+    # Pending Directions, such as Wedge, Words, Slide.
+     
+    for measure in measure_list:
+
+        new_elements = []
+        pre_note_directions = []
+        last_note_event = None
+
+        for elt in measure.elements:
+            # If elt is a NoteElement, check if there are any pending
+            # directions to collect
+            if is_note_or_chord(elt):
+                elt.directions = DirectionsRegister()
+                last_note_event = elt
+                # Attach Directions if their offset is greater than
+                # the offset of the current element. 
+                if pre_note_directions:
+                    insert, pre_note_directions = partition_by_offset(
+                        pre_note_directions, elt.measure_offset)
+                    elt.directions.extend(insert)
+                new_elements.append(elt)
+
+                print elt.directions.elements
+
+            elif is_direction(elt):
+                pre_note_directions.append(elt)
+
+        # At the end of the Measure, gather all pending Directions
+        # and attach them to the last active NoteElement
+        if pre_note_directions:
+            if last_note_event:
+                last_note_event.directions.extend(pre_note_directions)
+            pre_note_directions = [] 
+
+        measure.elements = new_elements
+        new_measure_list.append(measure)
+
+    return new_measure_list
