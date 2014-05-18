e696e4094140b29fd5626d6728b24c6e5e0149f1
fredrik wallberg <fredrik.wallberg@gmail.com>
2014-05-18 16:17:33 +0200

    Default to numeric time signature.

--- measure/time_signature.py
+++ measure/time_signature.py
@@ -1,11 +1,22 @@
 class TimeSignature(object):
 
+    styles = {
+        'default': "\\defaultTimeSignature",
+        'numeric': "\\numericTimeSignature",
+        }
+
     def __init__(self, numerator=None, denominator=None):
         self.numerator=numerator
         self.denominator=denominator
 
     @property
+    def time_signature_style(self):
+        # TODO: implement more styles 
+        return 'numeric'
+
+    @property
     def lilypond_format(self):
         if self.numerator and self.denominator:
-            return '\\time {num}/{denom}'.format(num=self.numerator,
-                                                 denom=self.denominator)
+            style = self.styles.get(self.time_signature_style, None)
+            result = '{style} \n \\time {num}/{denom}'
+            return result.format(style=style, num=self.numerator, denom=self.denominator)
