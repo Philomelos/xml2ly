ab59ddbeca2df3a18642de9b5851a925766a7228
fredrik wallberg <fredrik.wallberg@gmail.com>
2014-05-19 10:01:53 +0200

    Catch all attributes, including those occuring in the middle of a measure.

--- part/set_note_attributes.py
+++ part/set_note_attributes.py
@@ -1,14 +1,17 @@
 from copy import copy
 from music_data.attributes import ElementAttributes
+from resources.musicxml import attributes
 
 def set_note_attributes(measure_list):
 
     curr_attr = ElementAttributes()
 
     for measure in measure_list:
-        measure_attributes = measure.attributes[:]
-        curr_attr.update(measure_attributes)
         for elt in measure.elements:
-            # initialize attributes and directions
-            elt.attributes = copy(curr_attr)
-            elt.directions = None
+            if isinstance(elt, attributes):
+                measure_attributes = measure.attributes[:]
+                curr_attr.update(measure_attributes)
+            else:
+                # initialize attributes and directions
+                elt.attributes = copy(curr_attr)
+                elt.directions = None
